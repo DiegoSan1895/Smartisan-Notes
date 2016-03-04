faf46f2f128ff93e02f106882c9d1e28bf82a71b
SanDiego <xyq_since1895@163.com>
2016-03-04 13:56:07 +0800

    add monkey king

--- Smartisan-Notes/Smartisan-Notes/AboutViewController.swift
+++ Smartisan-Notes/Smartisan-Notes/AboutViewController.swift
@@ -21,20 +21,7 @@ class AboutViewController: UITableViewController {
     @IBOutlet var toastButton: UIButton!
     @IBOutlet weak var webTableViewCell: UITableViewCell!
     
-    struct AppURL {
-        static let clockURL = NSURL(string: "notes://")!
-        static let syncURL = NSURL(string: "huan-xi-yun-tong-bu-gong-ju://")!
-        static let calenderURL = NSURL(string: "smartisan-calendar://")!
         
-        static let smartisanWeb = NSURL(string: "https://store.smartisan.com")!
-    }
-    struct AppItunsURL {
-        static let calenderURL = NSURL(string: "https://itunes.apple.com/us/app/smartisan-calendar/id944154964?mt=8")!
-        static let syncURL = NSURL(string: "https://itunes.apple.com/cn/app/huan-xi-yun-tong-bu-gong-ju/id880078620?mt=8")!
-        static let clockURL = NSURL(string: "https://itunes.apple.com/us/app/smartisan-clock/id828812911?mt=8")!
-        static let notesURL = NSURL(string: "https://itunes.apple.com/cn/app/chui-zi-bian-qian/id867934588?mt=8")!
-    }
-    
     struct SegueIdentifier{
         static let wechat = "wechat"
         static let weibo = "weibo"
